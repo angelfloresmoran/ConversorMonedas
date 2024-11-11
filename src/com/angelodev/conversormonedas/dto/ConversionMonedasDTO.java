a98d15c092a05b478c445a405c318a2c9134edea
Luis Angel Flores <angel.steph.3006@gmail.com>
2024-11-11 16:39:08 -0600

    Actualizacion del achivo README,md, se agrego la descripcion e insignia de finalizacion

--- src/com/angelodev/conversormonedas/dto/ConversionMonedasDTO.java
+++ src/com/angelodev/conversormonedas/dto/ConversionMonedasDTO.java
@@ -1,7 +1,5 @@
 package com.angelodev.conversormonedas.dto;
 
-import com.angelodev.conversormonedas.modelos.Moneda;
-
 public record ConversionMonedasDTO(String base_code, String target_code,  double conversion_rate, double conversion_result) {
     @Override
     public String toString() {
